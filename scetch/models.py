47f650f53d8d6804637c30deebe3760a623c5e31
Begislan <begislanbaysatanuulu2001@gmail.com>
2025-03-28 00:09:23 +0600

    version2

--- scetch/models.py
+++ scetch/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from django.conf import settings
 
+User = settings.AUTH_USER_MODEL
 class QueueSlot(models.Model):
     date = models.DateField()
     start_time = models.TimeField()
